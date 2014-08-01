18f217144774fc77f9ac32a88fe6ffbe2957b61b
Novanet- <gamemaster8888@gmail.com>
2014-08-01 17:07:46 +0100

    Removed history from tracking, added more detail/colours to main screen

--- MainScreen.pas
+++ MainScreen.pas
@@ -4,10 +4,19 @@ interface
 
 uses
   Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
-  Dialogs;
+  Dialogs, ExtCtrls, StdCtrls, Buttons, ComCtrls;
 
 type
   TfrmMainScreen = class(TForm)
+    edtPlainText: TRichEdit;
+    edtCipherText: TRichEdit;
+    btnPlainToCipher: TBitBtn;
+    btnCipherToPlain: TBitBtn;
+    pnlHeader: TPanel;
+    pnlBody: TPanel;
+    lblTitle: TLabel;
+    lblPlainText: TLabel;
+    lblCipherText: TLabel;
   private
     { Private declarations }
   public
@@ -20,5 +29,7 @@ var
 implementation
 
 {$R *.dfm}
+
+begin
 
 end.
